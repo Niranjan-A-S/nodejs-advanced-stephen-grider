2dd349ba789d4beb03d0bdea604e11567ec9e8aa
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-06-03 11:31:24 +0530

    clustering setup

--- performance/index.js
+++ performance/index.js
@@ -1,16 +1,32 @@
+const cluster = require('node:cluster');
 const express = require('express');
+const os = require('node:os');
 app = express();
 
-function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) { }
+//Checking if the cluster is in master mode
+if (cluster.isMaster) {
+    //run the index.js file again in child mode
+    let index = 0;
+    while (index++ < os.availableParallelism()) {
+        cluster.fork();
+    }
+} else {
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) { }
+    }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there')
+    })
+
+    app.get('/fast', (req, res) => {
+        return res.send('This is a fast response');
+    });
+
+    app.listen(3000, () => {
+        console.log('Server started running on port 3000.')
+    });
+
 }
-
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there')
-})
-
-app.listen(3000, () => {
-    console.log('Server started running on port 3000.')
-}) 
