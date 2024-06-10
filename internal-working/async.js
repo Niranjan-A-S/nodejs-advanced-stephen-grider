885ddfa0abeea006cd47abb5f46e4d5ed59a54c7
Niranjan A S <niranjan0881@gmail.com>
2024-06-10 13:54:23 +0530

    chore: notes on os task

--- internal-working/async.js
+++ internal-working/async.js
@@ -20,3 +20,4 @@ makeNetworkRequest();
 //here all 6 tasks completed simultaneously
 //Here the OS does the real HTTP request the task is getting allocated to the OS through the libuv
 //There has been signalling done to receive the response in the libuv and ..
+//Libuv instead delegate the work to the OS kernel and whenever possible, it will poll the kernel and see if the request has completed
